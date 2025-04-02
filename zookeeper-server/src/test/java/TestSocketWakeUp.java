b4e57047a0837c5bdfad073b611470e296d23445
xueguichen <long1234@MG>
2025-04-02 18:50:20 +0800

    read

--- zookeeper-server/src/test/java/TestSocketWakeUp.java
+++ zookeeper-server/src/test/java/TestSocketWakeUp.java
@@ -15,7 +15,7 @@ import java.util.Set;
  * @since 2025/4/1
  */
 
-public class TestSocketWakeUp {
+public class TestSocketWakeUp {  /*  selector.wakeup() 唤醒 selector.select */
     static Logger LOG = LoggerFactory.getLogger(ZooKeeper.class);
 
     public static void main(String[] args) throws Exception{
@@ -25,16 +25,14 @@ public class TestSocketWakeUp {
         InetSocketAddress serverAddress = new InetSocketAddress("192.168.229.130", 2181);
         SocketChannel channel = SocketChannel.open();
         channel.configureBlocking(false); // 非阻塞模式
-        channel.connect(serverAddress);
-
-        SelectionKey selectionKey = channel.register(selector, SelectionKey.OP_CONNECT);
+        boolean connect = channel.connect(serverAddress);
 
         new Thread(() -> {
             while (true) {
-                LOG.info("Thread 1: select() blocking...");
+                LOG.info("SocketChannel select() blocking...");
                 try {
                     int ready = selector.select(10000); // 阻塞直到事件就绪或超时
-                    LOG.info("Thread 1: select() returned with " + ready + " ready channels");
+                    LOG.info("SocketChannel select() returned with " + ready + " ready channels");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -47,17 +45,17 @@ public class TestSocketWakeUp {
                         if (key.isConnectable()) {
                             SocketChannel ch = (SocketChannel) key.channel();
                             if (ch.finishConnect()) {
-                                LOG.info("Thread 1: 连接成功...");
-                                key.interestOps(SelectionKey.OP_WRITE); // 注册写事件
+                                LOG.info("SocketChannel 连接成功...");
+                                //key.interestOps(SelectionKey.OP_WRITE); // 注册写事件
                             } else {
-                                LOG.error("连接失败，关闭通道");
+                                LOG.error("SocketChannel关闭通道");
                                 ch.close();
                             }
                         } else if (key.isWritable()) {
-                            LOG.info("Thread 1: isWritable...");
+                            LOG.info("SocketChannel isWritable...");
                             // 写入数据...
                         } else if (key.isReadable()) {
-                            LOG.info("Thread 1: isReadable...");
+                            LOG.info("SocketChannel isReadable...");
                         }
                     } catch (IOException e) {
                         LOG.error("处理事件异常", e);
@@ -77,12 +75,12 @@ public class TestSocketWakeUp {
 
             while (true){
                 try {
-                    Thread.sleep(10000);
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
                 LOG.info("Thread 2: calling wakeup()");
-               // selector.wakeup(); // 唤醒 select()，但不会添加任何 SelectionKey
+               selector.wakeup(); // 唤醒 select()
             }
 
         }).start();
